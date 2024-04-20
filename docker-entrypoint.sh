50c5bef268b0b9b9c2fd94924916455bdb89e71d
Edward Lee <edwardlee@git.mylab.zzz>
2024-04-21 01:44:48 +0800

    Added more check for first argv

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,47 +1,52 @@
 #!/bin/sh
 
-# Create SSH keys directory if needed
-keys_dir=/etc/ssh/keys
-if [ ! -d "$keys_dir" ]; then
-    mkdir "$keys_dir"
+# Check argument first
+if [ "/usr/sbin/sshd" = "${1}" ]; then
+    is_sshd=1
+else
+    is_sshd=0
 fi
 
-# Setup SSH HostKey if needed
+# Setup SSH daemon
+keys_dir=/etc/ssh/keys
+if [ $is_sshd -eq 1 ] && [ ! -d "$keys_dir" ]; then
+    mkdir "$keys_dir"
+fi
 key_file="${keys_dir}/ssh_host_ed25519_key"
-conf_dir=/etc/ssh/sshd_config.d
-if [ ! -f "$key_file" ]; then
+if [ $is_sshd -eq 1 ] && [ ! -f "$key_file" ]; then
     echo "Generating SSH host key $key_file..."
     ssh-keygen -q -N '' -f "$key_file" -t ed25519
 fi
-if ! grep -q "HostKey $key_file" "${conf_dir}/20_hostkeys.conf" ; then
+con_file=/etc/ssh/sshd_config.d/20_hostkeys.conf
+if [ $is_sshd -eq 1 ] && ! grep -q "HostKey $key_file" "${con_file}"; then
     echo "Adding SSH host key $key_file..."
-    echo "HostKey $key_file" >> "${conf_dir}/20_hostkeys.conf"
+    echo "HostKey $key_file" >> "${con_file}"
 fi
 
 # Setup gitolite admin
 auth_keys=~git/.ssh/authorized_keys
-if [ ! -f "$auth_keys" ] && [ ! -n "$ADMIN_KEY" ] && [ "${1}" = "/usr/sbin/sshd" ]; then
+if [ $is_sshd -eq 1 ] && [ ! -f "$auth_keys" ] && [ ! -n "$ADMIN_KEY" ]; then
     echo "You need to specify ADMIN_KEY on first run to setup gitolite"
     echo 'Examples:'
     echo '    docker run --env ADMIN_KEY="$(cat ~/.ssh/id_rsa.pub)" jgiannuzzi/gitolite'
     exit 1
 fi
-if [ ! -f "$auth_keys" ] && [ -n "$ADMIN_KEY" ] && [ "${1}" = "/usr/sbin/sshd" ]; then
+if [ $is_sshd -eq 1 ] && [ ! -f "$auth_keys" ] && [ -n "$ADMIN_KEY" ]; then
     echo "Initialling gitolite with public key [${ADMIN_KEY}]..."
     echo "$ADMIN_KEY" | tr -d "\n" > "/dev/shm/admin.pub"
     su - git -c "gitolite setup --pubkey /dev/shm/admin.pub"
     rm "/dev/shm/admin.pub"
 fi
 
-# Clean temp variables
-unset keys_dir key_file conf_dir auth_keys
-
 # Check setup and fix permissions at every sshd startup
-if [ "${1}" = "/usr/sbin/sshd" ]; then
+if [ $is_sshd -eq 1 ]; then
     echo "Checking gitolite initialization..."
     su - git -c "gitolite setup"
     echo "Fixing gitolite data permissions..."
     chown -R git:git ~git
 fi
+
+# Clean temp variables
+unset is_sshd keys_dir key_file con_file auth_keys
 
 exec "$@"
