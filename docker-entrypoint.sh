5c4e3eccea3cd2184829f25ae99aa5b8b42593bc
Edward Lee <edwardlee@git.mylab.zzz>
2024-12-30 15:13:03 +0800

    Fixed gitolite-admin repo not work for default branch name change

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -70,8 +70,10 @@ fi
 if [ $is_sshd -eq 1 ] && [ ! -f "$auth_keys" ] && [ -n "$ADMIN_KEY" ]; then
     echo '> Initialling gitolite admin repository...'
     echo "$ADMIN_KEY" | tr -d "\n" > /dev/shm/admin.pub
+    su - git -c 'git config --global init.defaultBranch master'
     su - git -c 'gitolite setup --pubkey /dev/shm/admin.pub'
     rm /dev/shm/admin.pub
+    rm ~git/.gitconfig
     echo '> done'
 fi
 
